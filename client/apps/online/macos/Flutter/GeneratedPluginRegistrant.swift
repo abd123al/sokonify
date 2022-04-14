c5bc9ccd172f2e39ca420270fdb219e5782d2915
Sylvanus Kateile <kateile@outlook.com>
2022-04-14 12:43:12 +0300

    add online app template

---
+++ client/apps/online/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import graph
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GraphPlugin.register(with: registry.registrar(forPlugin: "GraphPlugin"))
+}
