cf7b7504a7139985cf118b6d81b6bdab9673a3e2
Sylvanus Kateile <kateile@outlook.com>
2022-04-21 19:16:47 +0300

    server: implement create payment via transaction

--- server/graph/schema.resolvers.go
+++ server/graph/schema.resolvers.go
@@ -74,32 +74,44 @@ func (r *mutationResolver) CreateOrder(ctx context.Context, input model.OrderInp
 	return &order, result.Error
 }
 
-func (r *mutationResolver) CreatePayment(ctx context.Context, input model.PaymentInput) (*model.Payment, error) {
-	//todo should the order creator be the one to confirm payment.
-	var orderItems []model.OrderItem
-	var subPrice decimal.Decimal
+func (r *mutationResolver) CreatePayment(_ context.Context, input model.PaymentInput) (*model.Payment, error) {
+	var payment *model.Payment
 
-	r.DB.Where(&model.OrderItem{OrderID: input.OrderID}).Find(&orderItems)
+	err := r.DB.Transaction(func(tx *gorm.DB) error {
+		//todo should the order creator be the one to confirm payment.
+		var orderItems []model.OrderItem
+		var subPrice decimal.Decimal
 
-	for _, o := range orderItems {
-		price, err := decimal.NewFromString(o.Price)
-		if err != nil {
-			panic(err)
+		tx.Where(&model.OrderItem{OrderID: input.OrderID}).Find(&orderItems)
+
+		for _, o := range orderItems {
+			price, err := decimal.NewFromString(o.Price)
+			if err != nil {
+				panic(err)
+			}
+			total := price.Mul(decimal.NewFromInt(int64(o.Quantity)))
+
+			subPrice = subPrice.Add(total)
 		}
-		total := price.Mul(decimal.NewFromInt(int64(o.Quantity)))
 
-		subPrice = subPrice.Add(total)
-	}
+		payment = &model.Payment{
+			OrderID:     input.OrderID,
+			Description: input.Description,
+			ReferenceID: input.ReferenceID,
+			Type:        input.Type,
+			Amount:      subPrice.String(),
+		}
 
-	payment := model.Payment{
-		OrderID:     input.OrderID,
-		Description: input.Description,
-		ReferenceID: input.ReferenceID,
-		Type:        input.Type,
-		Amount:      subPrice.String(),
-	}
-	result := r.DB.Create(&payment)
-	return &payment, result.Error
+		// do some database operations in the transaction (use 'tx' from this point, not 'db')
+		if err := tx.Create(&payment).Error; err != nil {
+			return err
+		}
+
+		// return nil will commit the whole transaction
+		return nil
+	})
+
+	return payment, err
 }
 
 func (r *mutationResolver) CreateProduct(ctx context.Context, input model.ProductInput) (*model.Product, error) {
