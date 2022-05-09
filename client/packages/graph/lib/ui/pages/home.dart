8ddf56ec3906677cb00343e9ff0d7279d1146e0b
Sylvanus Kateile <kateile@outlook.com>
2022-05-09 11:12:20 +0300

    client: use BottomNavyBar

--- client/packages/graph/lib/ui/pages/home.dart
+++ client/packages/graph/lib/ui/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,14 +13,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Stores'),
+        title: InkWell(
+          child: const Text("Mwanana Pharmacy"),
+          onTap: () {
+            //todo show store picker.
+          },
+        ),
+        centerTitle: true,
         actions: [
           IconButton(
-            icon: const Icon(Icons.shopping_cart),
+            icon: const Icon(Icons.qr_code_outlined),
             tooltip: 'Open shopping cart',
             onPressed: () {
               BlocProvider.of<AuthCubit>(context).logOut();
@@ -27,7 +49,44 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: const StoresPage(),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            const StoresPage(),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+              title: const Text('Item One'), icon: const Icon(Icons.home)),
+          BottomNavyBarItem(
+              title: const Text('Item Two'), icon: const Icon(Icons.apps)),
+          BottomNavyBarItem(
+              title: const Text('Item Three'),
+              icon: const Icon(Icons.chat_bubble)),
+          BottomNavyBarItem(
+              title: const Text('Item Four'), icon: const Icon(Icons.settings)),
+        ],
+      ),
     );
   }
 }
