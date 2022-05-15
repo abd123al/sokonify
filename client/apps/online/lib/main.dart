ae7752e53511b7912c5cb5b53fd9eca79d1d462c
Sylvanus Kateile <kateile@outlook.com>
2022-05-15 20:58:41 +0300

    client: add some logic for configuring base url

--- client/apps/online/lib/main.dart
+++ client/apps/online/lib/main.dart
@@ -1,5 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:graph/graph.dart';
 
 void main() async {
-  await startApp("http://10.0.2.2:8080/graphql");
+  String getBaseUrl() {
+    if (kReleaseMode) {
+      return "http://127.0.0.1:8081";
+    } else {
+      //Web don't support platform
+      if (!kIsWeb) {
+        if (Platform.isAndroid) {
+          return "http://10.0.2.2:8080";
+        }
+      }
+    }
+
+    return "http://127.0.0.1:8080";
+  }
+
+  //This works for majority env
+  String baseUrl = String.fromEnvironment(
+    "BASE_URL",
+    defaultValue: getBaseUrl(),
+  );
+
+  await startApp("$baseUrl/graphql");
 }
