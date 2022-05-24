e94161a8e3335c86a9a297cc731fbf4ebb677a88
Sylvanus Kateile <kateile@outlook.com>
2022-05-24 17:51:02 +0300

    client: using OrientationLayoutBuilder in order_page.dart

--- client/packages/graph/lib/ui/pages/order/order_page.dart
+++ client/packages/graph/lib/ui/pages/order/order_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graph/ui/pages/order/print.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import '../../../gql/generated/graphql_api.graphql.dart';
 import '../../../repositories/order_repository.dart';
@@ -53,39 +54,69 @@ class OrderPage extends StatelessWidget {
           );
         }
 
-        return Scaffold(
-          body: ListView(
-            children: [
-              if (data.customer?.name != null)
-                _buildTile("Customer", "${data.customer?.name}"),
-              _buildTile("Created At", "${data.createdAt}"),
-              _buildTile("Status", describeEnum(data.status)),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: data.orderItems.length,
-                itemBuilder: (context, index) {
-                  final item = data.orderItems[index];
+        final left = [
+          if (data.customer?.name != null)
+            _buildTile("Customer", "${data.customer?.name}"),
+          _buildTile("Created At", "${data.createdAt}"),
+          _buildTile("Status", describeEnum(data.status)),
+        ];
 
-                  return ListTile(
-                    title: Text(item.price),
-                  );
-                },
+        final right = [
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: data.orderItems.length,
+            itemBuilder: (context, index) {
+              final item = data.orderItems[index];
+
+              return ListTile(
+                title: Text(item.price),
+              );
+            },
+          ),
+          Container(
+            color: Colors.blue.shade50,
+            child: ListTile(
+              title: Text(
+                "Total Amount",
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-              Container(
-                color: Colors.blue.shade50,
-                child: ListTile(
-                  title: Text(
-                    "Total Amount",
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  trailing: Text(
-                    "70000",
-                    style: Theme.of(context).textTheme.titleMedium,
+              trailing: Text(
+                "70000",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+          ),
+        ];
+
+        return Scaffold(
+          body: OrientationLayoutBuilder(
+            portrait: (context) => ListView(
+              children: [
+                ...left,
+                ...right,
+              ],
+            ),
+            landscape: (context) => Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    elevation: 16,
+                    child: ListView(
+                      children: left,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Card(
+                    elevation: 16,
+                    child: ListView(
+                      children: right,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           bottomNavigationBar: BottomNavigationBar(
             backgroundColor: Colors.grey.shade50,
