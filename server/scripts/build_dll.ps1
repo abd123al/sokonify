9a2ac0d36a98f71b62f394194c15bf70f1a28fac
Sylvanus Kateile <kateile@outlook.com>
2022-06-01 07:19:59 +0300

    server: add default timeout

--- server/scripts/build_dll.ps1
+++ server/scripts/build_dll.ps1
@@ -12,7 +12,9 @@ Set-Location $PSScriptRoot
 # Switching to server root
 cd..
 
+Write-Host "Start Building DDl...!"
+
 #Installing go and build
 go build -o ../client/packages/server/windows/lib.dll -buildmode=c-shared -ldflags="-s -w" ./lib.go
 
-Write-Host "Done!"
+Write-Host "Done building dll!"
