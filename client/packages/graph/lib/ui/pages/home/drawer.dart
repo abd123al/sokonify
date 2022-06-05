734db50b3bd22e12313cc2c48b52c4409eac3095
Sylvanus Kateile <kateile@outlook.com>
2022-06-05 21:37:34 +0300

    client: remove CachedNetworkImage

--- client/packages/graph/lib/ui/pages/home/drawer.dart
+++ client/packages/graph/lib/ui/pages/home/drawer.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
@@ -19,11 +18,11 @@ class SokonifyDrawer extends StatelessWidget {
           return UserAccountsDrawerHeader(
             accountEmail: Text(data.email),
             accountName: Text(data.name),
-            currentAccountPicture: CachedNetworkImage(
-              imageUrl: "",
-              placeholder: (__, _) => const CircularProgressIndicator(),
-              errorWidget: (___, __, _) => const Icon(Icons.error),
-            ),
+            // currentAccountPicture: CachedNetworkImage(
+            //   imageUrl: "",
+            //   placeholder: (__, _) => const CircularProgressIndicator(),
+            //   errorWidget: (___, __, _) => const Icon(Icons.error),
+            // ),
           );
         },
       ),
