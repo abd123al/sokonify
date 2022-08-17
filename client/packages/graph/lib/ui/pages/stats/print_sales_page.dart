a5b126df1149d79f00c508a3adea1116ee8c4942
Sylvanus Kateile <kateile@outlook.com>
2022-08-17 12:26:15 +0300

    client: add sales printing functionality

--- client/packages/graph/lib/ui/pages/stats/print_sales_page.dart
+++ client/packages/graph/lib/ui/pages/stats/print_sales_page.dart
@@ -1,10 +1,14 @@
 import 'package:blocitory/helpers/resource_query_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graph/ui/widgets/widgets.dart';
+import 'package:pdf/pdf.dart';
+import 'package:printing/printing.dart';
 
 import '../../../gql/generated/graphql_api.graphql.dart';
 import '../../../repositories/stats_repository.dart';
+import '../../pdf/sales_list.dart';
 import 'sales_cubit.dart';
 
 class PrintSalesPage extends StatelessWidget {
@@ -21,7 +25,7 @@ class PrintSalesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text("Print $word Sales"),
+        title: Text("Print $word Sales"),
       ),
       body: PermissionBuilder(
         type: PermissionType.viewStats,
@@ -41,7 +45,23 @@ class PrintSalesPage extends StatelessWidget {
         retry: (cubit) => cubit.fetch(args),
         initializer: (cubit) => cubit.fetch(args),
         builder: (context, data, _) {
-          return const SizedBox();
+          return StoreBuilder(
+            noBuilder: (context) => const SizedBox(),
+            builder: (context, store) {
+              return PdfPreview(
+                canDebug: kDebugMode,
+                pdfFileName: "${word.replaceAll(" ", "_")}.pdf",
+                initialPageFormat: PdfPageFormat.a4,
+                build: (PdfPageFormat format) {
+                  return generateSalesList(
+                    format,
+                    data,
+                    store,
+                  );
+                },
+              );
+            },
+          );
         },
       ),
     );
---
+++ client/packages/graph/lib/ui/pdf/sales_list.dart
@@ -0,0 +1,266 @@
+import 'dart:typed_data';
+
+import 'package:collection/collection.dart';
+import 'package:intl/intl.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+import '../../gql/generated/graphql_api.graphql.dart';
+
+Future<Uint8List> generateSalesList(
+  PdfPageFormat pageFormat,
+  List<Sales$Query$OrderItem> list,
+  CurrentStore$Query$Store store,
+) async {
+  final List<Sales$Query$OrderItem> items = list;
+  items.sort((a, b) => a.item.product.name.compareTo(b.item.product.name));
+
+  final invoice = Invoice(
+    store: store,
+    items: items,
+    baseColor: PdfColors.teal,
+    accentColor: PdfColors.blueGrey900,
+  );
+
+  return await invoice.buildPdf(pageFormat);
+}
+
+class Invoice {
+  Invoice({
+    required this.items,
+    required this.store,
+    required this.baseColor,
+    required this.accentColor,
+  });
+
+  final List<Sales$Query$OrderItem> items;
+  final CurrentStore$Query$Store store;
+  final PdfColor baseColor;
+  final PdfColor accentColor;
+
+  static const _darkColor = PdfColors.blueGrey800;
+
+  final borderRadius = const pw.BorderRadius.all(pw.Radius.circular(2));
+
+  Future<Uint8List> buildPdf(PdfPageFormat pageFormat) async {
+    final doc = pw.Document();
+
+    doc.addPage(
+      pw.MultiPage(
+        pageFormat: pageFormat,
+        footer: _buildFooter,
+        margin: const pw.EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 48,
+        ),
+        build: (context) => [
+          _buildHeader(context),
+          _contentTable(context),
+          pw.SizedBox(height: 20),
+          //if (isInventory) _buildBelow(context),
+        ],
+      ),
+    );
+
+    // Return the PDF file content
+    return doc.save();
+  }
+
+  pw.Widget _buildHeader(pw.Context context) {
+    return pw.Column(
+      children: [
+        pw.Row(
+          mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+          children: [
+            pw.Column(
+              crossAxisAlignment: pw.CrossAxisAlignment.start,
+              children: [
+                pw.Text(
+                  store.name,
+                  style: pw.TextStyle(
+                    fontWeight: pw.FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                pw.Text(
+                  store.description ??
+                      "Your facility \ndescription \nwill appear here...",
+                  style: const pw.TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            pw.Container(
+              width: 80,
+              height: 80,
+              child: pw.Text(""),
+            )
+          ],
+        ),
+        pw.Divider(),
+        pw.Container(
+          alignment: pw.Alignment.center,
+          child: pw.Text(
+            'Sales List',
+            style: pw.TextStyle(
+              fontWeight: pw.FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  pw.Widget _buildFooter(pw.Context context) {
+    return pw.Column(
+      crossAxisAlignment: pw.CrossAxisAlignment.start,
+      children: [
+        pw.SizedBox(height: 8),
+        pw.Row(
+          mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+          children: [
+            pw.Container(
+              child: pw.Text(
+                'Powered by Sokonify',
+                style: pw.TextStyle(
+                  fontSize: 12,
+                  fontStyle: pw.FontStyle.italic,
+                ),
+              ),
+            ),
+            pw.Container(
+              child: pw.Text(
+                _formatDate(DateTime.now()),
+                style: const pw.TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            pw.Container(
+              child: pw.Text(
+                'Page ${context.pageNumber} of ${context.pagesCount}',
+                style: pw.TextStyle(
+                  fontSize: 12,
+                  fontWeight: pw.FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  pw.Widget _contentTable(pw.Context context) {
+    List<String> headers = [
+      "#",
+      "Item Name",
+      "Unit",
+      "Unit Price",
+      "Quantity",
+      "Sub Total",
+    ];
+
+    final List<List<Object>> data = items.mapIndexed((i, e) {
+      List<Object> list = [
+        (i + 1),
+        "${e.item.product.name}${e.item.brand?.name != null ? " (${e.item.brand?.name})" : ""}",
+        (e.item.unit.name),
+        e.price,
+        e.quantity,
+        e.subTotalPrice,
+      ];
+
+      return list;
+    }).toList();
+
+    return pw.Table.fromTextArray(
+      border: null,
+      cellAlignment: pw.Alignment.centerLeft,
+      headerDecoration: pw.BoxDecoration(
+        border: pw.Border.all(
+          color: _darkColor,
+        ),
+        borderRadius: borderRadius,
+      ),
+      headerHeight: 25,
+      cellHeight: 40,
+      cellAlignments: {
+        0: pw.Alignment.centerLeft,
+        1: pw.Alignment.centerLeft,
+        2: pw.Alignment.centerLeft,
+        3: pw.Alignment.center,
+        4: pw.Alignment.center,
+        5: pw.Alignment.center,
+        6: pw.Alignment.centerRight,
+      },
+      headerStyle: pw.TextStyle(
+        fontSize: 10,
+        fontWeight: pw.FontWeight.bold,
+      ),
+      cellStyle: const pw.TextStyle(
+        color: _darkColor,
+        fontSize: 10,
+      ),
+      rowDecoration: pw.BoxDecoration(
+        border: pw.Border(
+          bottom: pw.BorderSide(
+            color: accentColor,
+            width: .5,
+          ),
+        ),
+      ),
+      headers: headers,
+      data: data,
+    );
+  }
+
+// pw.Widget _buildBelow(pw.Context context) {
+//   final total = calculateTotal(
+//     items.map(
+//       (e) => TotalPriceArgs(
+//         price: ItemTile.price(e, pricing.id),
+//         quantity: e.quantity,
+//       ),
+//     ),
+//   );
+//
+//   return pw.Column(
+//     crossAxisAlignment: pw.CrossAxisAlignment.start,
+//     children: [
+//       pw.Row(
+//         mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+//         children: [
+//           pw.Expanded(child: pw.SizedBox(height: 8)),
+//           pw.Container(
+//             child: pw.Text(
+//               'Total Cost',
+//               style: pw.TextStyle(
+//                 fontSize: 12,
+//                 fontStyle: pw.FontStyle.italic,
+//               ),
+//             ),
+//           ),
+//           pw.SizedBox(width: 32),
+//           pw.Container(
+//             child: pw.Text(
+//               total,
+//               style: pw.TextStyle(
+//                 fontSize: 12,
+//                 fontWeight: pw.FontWeight.bold,
+//               ),
+//             ),
+//           ),
+//         ],
+//       )
+//     ],
+//   );
+// }
+}
+
+String _formatDate(DateTime date) {
+  final format = DateFormat('d/M/y').add_Hms();
+  return format.format(date);
+}
