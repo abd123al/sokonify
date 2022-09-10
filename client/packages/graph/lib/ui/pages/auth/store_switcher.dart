e83c0d277774cfb30822b0ebd42edc07bb79692e
Sylvanus Kateile <kateile@outlook.com>
2022-09-10 17:44:18 +0300

    client: close store switcher dialog after switch

--- client/packages/graph/lib/ui/pages/auth/store_switcher.dart
+++ client/packages/graph/lib/ui/pages/auth/store_switcher.dart
@@ -7,6 +7,7 @@ import '../../../gql/generated/graphql_api.graphql.dart';
 import '../../../nav/nav.dart';
 import '../../../repositories/auth_repository.dart';
 import '../../widgets/auth_wrapper_cubit.dart';
+import '../../widgets/store_builder.dart';
 import '../store/stores_list_cubit.dart';
 import 'auth_cubit.dart';
 
@@ -22,82 +23,96 @@ class StoreSwitcher extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return QueryBuilder<ResourceListData<Stores$Query$Store>, StoresListCubit>(
-      retry: (cubit) => cubit.fetch(),
-      builder: (context, stores, _) {
-        return MutationBuilder<AuthPartsMixin, LoginCubit, AuthRepository>(
-          blocCreator: (r) => LoginCubit(r),
-          onSuccess: (context, data) {
-            BlocProvider.of<AuthWrapperCubit>(context).login(data);
+    return StoreBuilder(
+      noBuilder: (BuildContext ctx) {
+        return const SizedBox();
+      },
+      builder: (context, current) {
+        return QueryBuilder<ResourceListData<Stores$Query$Store>,
+            StoresListCubit>(
+          retry: (cubit) => cubit.fetch(),
+          builder: (context, stores, _) {
+            return MutationBuilder<AuthPartsMixin, LoginCubit, AuthRepository>(
+              blocCreator: (r) => LoginCubit(r),
+              onSuccess: (context, data) {
+                BlocProvider.of<AuthWrapperCubit>(context).login(data);
 
-            //Restart app.
-            Phoenix.rebirth(context);
-          },
-          loadingWidget: const Center(
-            child: CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          ),
-          builder: (context, cubit) {
-            if (builder != null) {
-              return builder!(context, cubit);
-            }
+                Navigator.pop(context);
 
-            return ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Center(
-                    child: Text(
-                      "Please select the facility you want to switch to",
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ),
+                //Restart app.
+                Phoenix.rebirth(context);
+              },
+              loadingWidget: const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
                 ),
-                ListView.separated(
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    final store = stores.items[index];
+              ),
+              builder: (context, cubit) {
+                if (builder != null) {
+                  return builder!(context, cubit);
+                }
 
-                    return ListTile(
-                      title: Text(
-                        store.name,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
+                return ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Center(
+                        child: Text(
+                          "Please select the facility you want to switch to",
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                      ),
+                    ),
+                    ListView.separated(
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        final store = stores.items[index];
+
+                        final isCurrent = current.id == store.id;
+                        return ListTile(
+                          title: Text(
+                            store.name,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          trailing: isCurrent
+                              ? const Icon(Icons.verified)
+                              : const Icon(Icons.arrow_forward_sharp),
+                          onTap: () {
+                            cubit.switchStore(
+                              SwitchStoreInput(storeId: store.id),
+                            );
+                          },
+                          enabled: !isCurrent,
+                        );
+                      },
+                      itemCount: stores.items.length,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const Divider();
+                      },
+                    ),
+                    Center(
+                      child: Text(
+                        "or",
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ),
+                    TextButton(
+                      child: const Text(
+                        "Create New Facility",
+                        style: TextStyle(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      trailing: const Icon(Icons.arrow_forward_sharp),
-                      onTap: () {
-                        cubit.switchStore(
-                          SwitchStoreInput(storeId: store.id),
-                        );
+                      onPressed: () {
+                        redirectTo(context, Routes.createStore);
                       },
-                    );
-                  },
-                  itemCount: stores.items.length,
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const Divider();
-                  },
-                ),
-                Center(
-                  child: Text(
-                    "or",
-                    style: Theme.of(context).textTheme.titleSmall,
-                  ),
-                ),
-                TextButton(
-                  child: const Text(
-                    "Create New Facility",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  onPressed: () {
-                    redirectTo(context, Routes.createStore);
-                  },
-                )
-              ],
+                    )
+                  ],
+                );
+              },
             );
           },
         );
